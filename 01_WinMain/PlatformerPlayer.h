8f05bbe8eb78c19782e9bdffe6ff4b75b868b650
jong sung kim <cwos17@naver.com>
2021-02-20 23:29:32 +0900

    플랫포머플레이어 상태추가

--- 01_WinMain/PlatformerPlayer.h
+++ 01_WinMain/PlatformerPlayer.h
@@ -4,7 +4,32 @@ enum struct PlayerState : int
 {
 	RightIdle,
 	LeftIdle,
-	
+	LeftDownIdle,
+	RightDownIdle,
+	RightRun,
+	LeftRun,
+	LeftJump,
+	RightJump,
+	RightDown,
+	LeftDown,
+	RightDownShot,
+	LeftDownShot,
+	RightShot,
+	LeftShot,
+	LeftRunShot,
+	RightSpecialAttack,
+	LeftSpecialAttack,
+	UpSpecialAttack,
+	DownSpecialAttack,
+	RightRunShot,
+	RightGroundHit,//오른쪽땅에서 피격
+	RightAirHit, // 오른쪽 공중 피격
+	LeftGroundHit,
+	LeftAirHit,
+	LeftDash,
+	RightDash,
+	Die,
+
 };
 class PlatformerPlayer : public GameObject
 {
