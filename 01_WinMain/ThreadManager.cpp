8bc96ce90b648dceb948e921ee4922e5fd828665
HJJ1007 <73575357+HJJ1007@users.noreply.github.com>
2021-02-25 18:33:45 +0900

    ThreadManager
    ThreadManager 수정

--- 01_WinMain/ThreadManager.cpp
+++ 01_WinMain/ThreadManager.cpp
@@ -11,33 +11,37 @@ void ThreadManager::LoadStartFunts()
 	if (mLoadingThread == nullptr) {
 		function<void(void)> threadFunc = bind(&ThreadManager::Loding, this);
 		//스레드는 메모리 할당해주는 순간부터 바로 돌기 시작한다.
+		mIsPlay = true;
 		mLoadingThread = new thread(threadFunc);
 	}
 }
 
 void ThreadManager::LoadStartEnd()
 {
+	mLoadingThread->join();
 	delete mLoadingThread;
 	mLoadingThread = nullptr;
+	mIsThreadPlay = false;
 }
 
 void ThreadManager::Loding() {
-	if (mIsPlay) {
-		function<void(void)> startFunc = mFunts.front();
-		startFunc();
-		mFunts.pop();
-	}
-}
-
-void ThreadManager::Update() {
-	if (mIsPlay) {
-		if (mFunts.size() == 0) {
-			mIsThreadPlay = false;
-
-		}
-		else if (mFunts.size() > 0) {
-			mIsThreadPlay = true;
-			LoadStartFunts();
+	while (mFunts.size() != 0) {
+		if (mIsPlay) {
+			function<void(void)> startFunc = mFunts.front();
+			startFunc();
+			mFunts.pop();
 		}
 	}
+	LoadStartEnd();
 }
+
+//void ThreadManager::Update() {
+//	if (mIsPlay) {
+//		if (mFunts.size() == 0) {
+//			mIsThreadPlay = false;
+//		}
+//		else if (mFunts.size() > 0) {
+//			mIsThreadPlay = true;
+//		}
+//	}
+//}
--- 01_WinMain/ThreadManager.h
+++ 01_WinMain/ThreadManager.h
@@ -11,7 +11,6 @@ public :
 	void LoadStartFunts();
 	void SetFunts(const function<void(void)>& funts);
 	void Loding();
-	void Update();
 	void LoadStartEnd();
 
 	void SetIsPlay(bool d) { mIsPlay = d; }
